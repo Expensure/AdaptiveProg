725f59d8d1637588d0192d3cabf656c1bcc1f41f
Expensure <60733685+Expensure@users.noreply.github.com>
2020-04-19 12:52:33 +0200

    Bugfix
    Fixed Autohuur so it would actually create a return value when a variable = 0

---
+++ Autohuur.java
@@ -0,0 +1,57 @@
+package Autohuur;
+
+public class Autohuur {
+	private Integer aantalDagen;
+	private Auto auto;
+	private Klant klant;
+		
+	public void setGehuurdeAuto(Auto gA) {
+		auto = gA;
+	}
+		
+	public void setHuurder(Klant k) {
+		klant = k;
+	}
+		
+	public void setAantalDagen(Integer aD) {
+	    aantalDagen = aD;
+	}
+	
+	public Auto getGehuurdeAuto() {
+		return auto;
+	}
+	
+	public Klant getHuurder() {
+		return klant;
+	}
+	
+	public double totaalPrijs() {
+		double totaalprijs = aantalDagen*auto.prijsPerDag - aantalDagen*auto.prijsPerDag*(klant.kortingsPercentage/100);
+		return totaalprijs;
+	}
+
+	
+	public String toString() {
+		String a = "";	
+        if (getGehuurdeAuto() == null) {
+        	a = "\t er is geen auto bekend \n";
+        } else {
+        	a = "\t autotype: " + getGehuurdeAuto().toString() + "\n";
+        }
+
+        if (getHuurder() == null) {
+            a = a + "\t er is geen huurder bekend \n";
+        } else {
+            a = a + "\t " + getHuurder().toString() + "\n";
+        }
+
+        if (aantalDagen == null) {
+            a = a + "\t aantal dagen: 0 en dat kost 0.0 \n";
+        } else {
+            a = a + "\t aantal dagen: " + this.aantalDagen + " en dat kost " + totaalPrijs() + "\n";
+        }
+        
+		return a;
+
+	}
+}
