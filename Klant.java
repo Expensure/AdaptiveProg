22649f25a0f5004b5d303f0a19ee244015ddf75a
Expensure <60733685+Expensure@users.noreply.github.com>
2020-04-19 12:51:11 +0200

    Added links to Main.java

---
+++ Klant.java
@@ -0,0 +1,23 @@
+package Autohuur;
+
+public class Klant {
+	private String naam;
+	double kortingsPercentage;
+
+	public Klant(String nm) {
+		naam = nm;
+	}
+
+	public double getKorting() {
+		return kortingsPercentage;
+	}
+
+	public void setKorting(double kP) {
+		kortingsPercentage = kP;
+	}
+
+	public String toString() {
+		String k = "op naam van: " + naam + "(korting: " + kortingsPercentage + "%)";
+		return k;
+	}
+}
